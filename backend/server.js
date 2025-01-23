0140b13dfdd439465411734a3346ba9009d8ce90
unknown <raghvendasingh7546@gmail.com>
2025-01-23 14:13:55 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,22 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const db = require("./config/db");
-const authRoutes = require("./routes/authRoutes")
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET, POST",
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
-app.use("/auth", authRoutes)
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
